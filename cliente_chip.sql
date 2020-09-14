aab9bc0b7b3877fd86c9f8a48e83f8fe83c6986e
AnaJu <56701827+AnaJL@users.noreply.github.com>
2020-09-14 19:14:46 -0300

    Update cliente_chip.sql

--- cliente_chip.sql
+++ cliente_chip.sql
@@ -1,11 +1,11 @@
 
-insert into cliente_chip (idNumero, idCliente) values ('', 94);
-insert into cliente_chip (idNumero, idCliente) values ('', 53);
-insert into cliente_chip (idNumero, idCliente) values ('', 87);
-insert into cliente_chip (idNumero, idCliente) values ('', 52);
-insert into cliente_chip (idNumero, idCliente) values ('', 21);
-insert into cliente_chip (idNumero, idCliente) values ('', 77);
-insert into cliente_chip (idNumero, idCliente) values ('', 25);
-insert into cliente_chip (idNumero, idCliente) values ('', 44);
-insert into cliente_chip (idNumero, idCliente) values ('', 11);
-insert into cliente_chip (idNumero, idCliente) values ('', 76);
+insert into cliente_chip (idNumero, idCliente) values ('83985112345', 1);
+insert into cliente_chip (idNumero, idCliente) values ('21985112346', 2);
+insert into cliente_chip (idNumero, idCliente) values ('95985212345', 3);
+insert into cliente_chip (idNumero, idCliente) values ('85985112346', 4);
+insert into cliente_chip (idNumero, idCliente) values ('63985212345', 10);
+insert into cliente_chip (idNumero, idCliente) values ('85985112345', 9);
+insert into cliente_chip (idNumero, idCliente) values ('83985212346', 8);
+insert into cliente_chip (idNumero, idCliente) values ('84985112345', 7);
+insert into cliente_chip (idNumero, idCliente) values ('83985212345', 6);
+insert into cliente_chip (idNumero, idCliente) values ('98985212345', 5);
