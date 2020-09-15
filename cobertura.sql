f03db3535b56039b5426329481253c1d0b151322
Bielsaar <bielsaar@gmail.com>
2020-09-15 15:19:38 -0300

    Limpeza e organização do repositório
    Arquivos sobressalentes apagados, arquivos restantes organizados.

--- cobertura.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO cobertura (uf, ddd) VALUES ('PB', 83);
-INSERT INTO cobertura (uf, ddd) VALUES ('PE', 81);
-INSERT INTO cobertura (uf, ddd) VALUES ('RN', 84);
-INSERT INTO cobertura (uf, ddd) VALUES ('SP', 11);
-INSERT INTO cobertura (uf, ddd) VALUES ('RJ', 21);
-INSERT INTO cobertura (uf, ddd) VALUES ('MG', 31);
