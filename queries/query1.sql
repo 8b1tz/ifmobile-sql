1cdc8e25e248c2ff6b709d24f57dcf528ea67378
NoName <53948477+8b1tz@users.noreply.github.com>
2020-09-15 18:31:40 -0300

    Update query1.sql

--- queries/query1.sql
+++ queries/query1.sql
@@ -1,7 +1,8 @@
-SELECT li.data, li.chip_emissor, li.duracao
+SELECT li.data as data_da_ligacao, li.chip_receptor as número_de_destino, li.duracao as duração
 FROM ligacao li 
-JOIN auditoria au 
-ON au.idnumero = li.chip_emissor
+JOIN chip chi
+ON li.chip_emissor = chi.idnumero join
+auditoria au ON au.idnumero = chi.idnumero
 WHERE (chip_emissor LIKE '83985112345')AND au.dataInicio <  li.data
 AND au.dataTermino > li.data
 ORDER BY li.data ASC;
