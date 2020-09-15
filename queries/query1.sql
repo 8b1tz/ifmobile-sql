b44bc1ae8c8014497ba0a587e87879d1d959371f
NoName <53948477+8b1tz@users.noreply.github.com>
2020-09-15 18:19:10 -0300

    Update query1.sql

--- queries/query1.sql
+++ queries/query1.sql
@@ -1,13 +1,7 @@
-SELECT li.data, li.chip_receptor, li.duracao
+SELECT li.data, li.chip_emissor, li.duracao
 FROM ligacao li 
-JOIN chip
-ON li.chip_emissor = chip.idnumero
 JOIN auditoria au 
-ON au.idnumero = chip.idnumero
-WHERE date_part('year',age(au.dataTermino, au.dataInicio)) = (SELECT MAX(date_part('year',age(au.dataTermino, au.dataInicio))) 
-                                                              FROM auditoria au
-                                                              JOIN chip
-                                                              ON au.idnumero = chip.idnumero
-                                                              JOIN ligacao li
-                                                              ON li.chip_emissor = chip.idnumero
-                                                              WHERE (chip_emissor LIKE '83985112345'));
+ON au.idnumero = li.chip_emissor
+WHERE (chip_emissor LIKE '83985112345')AND au.dataInicio <  li.data
+AND au.dataTermino > li.data
+ORDER BY li.data ASC;
