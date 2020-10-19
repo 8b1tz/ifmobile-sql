632c6994cc10dc779ebd53fb5d19311f95d1aeea
Bielsaar <bielsaar@gmail.com>
2020-10-19 14:02:38 -0300

    Atualizado baseado na correção
    Faltou a inclusão das datas extremas na tarefa 2.1

--- TAREFA 1/queries/query1.sql
+++ TAREFA 1/queries/query1.sql
@@ -25,8 +25,8 @@ JOIN auditoria AS au
 ON au.idnumero = chi.idnumero
 
 WHERE (chip_emissor LIKE '83985112345') 
-    AND (au.dataInicio <  li.data) 
-    AND (au.dataTermino > li.data)
+    AND (au.dataInicio <=  li.data) 
+    AND (au.dataTermino >= li.data)
 ORDER BY li.data;
 
 -- Ponto 2.2)
