3adb2299b39fea7328ad22f0378e6c138890b84e
Bielsaar <bielsaar@gmail.com>
2020-10-28 18:43:46 -0300

    Adicionada documentação e função de numeros aleatorios

--- PROJETO FINAL/Scripts/ponto1.sql
+++ PROJETO FINAL/Scripts/ponto1.sql
@@ -1,26 +1,36 @@
--- Cria função para inserir em  chip
+-- Criando função para gerar numero aleatorio de forma prática
+CREATE OR REPLACE FUNCTION random_between(low INT ,high INT) 
+   RETURNS INT AS
+$$
+BEGIN
+   RETURN floor(random()* (high-low + 1) + low);
+END;
+$$ language 'plpgsql' STRICT;
+
+--Função de geração de numeros
 CREATE OR REPLACE FUNCTION insere_Num()
 RETURNS TRIGGER AS $$
 DECLARE 
-    numero char(11);
-    numdupli char(11);
+	numero char(11);
+	numdupli char(11);
     num char(11);
     opera char(2);
     subnum char(11);
     random char;
-    uf char(4);
-    cursorNum refcursor;
-    rec RECORD;
-    dupli int;
-    idrandom int;
+	uf char(4);
+	cursorNum refcursor;
+	rec RECORD;
+	dupli int;
+	idrandom int;
 BEGIN
 num = '00000000000';
 subnum =  SUBSTR (num, 7, 4); 
-idrandom = floor(random()* (27) + 1);
+idrandom = random_between(1,27);
 uf =  ddd FROM estado WHERE idregiao = idrandom LIMIT 1;
 random = FLOOR((random()*2)+1);
 dupli = 0;
 
+-- CASE para adicionar o ddd da operadora pelo id dela
 CASE NEW.idoperadora
     WHEN  1 THEN opera = '83';
     WHEN  2 THEN opera =  '85';
@@ -35,22 +45,25 @@ CASE NEW.idoperadora
     ELSE opera = '82';
 END CASE;
 LOOP 
+	-- LOOP para garantir que o numero não acababe com 0000 e que não seja duplicado
     EXIT WHEN subnum != '0000' AND dupli = 0 ;
-        num =  round(random()* 9)::text|| LPAD(round((random() * 9998) + 1)::text, 4, '0');
-        dupli = 0;
+        num =  random_between(0,9)|| LPAD(random_between(1,9999)::text, 4, '0');
+		dupli = 0;
         subnum =  SUBSTR (num, 7);
-        numdupli = uf || '9' || opera || random || num;
-        OPEN cursorNum NO SCROLL FOR SELECT idnumero FROM chip;
-        LOOP
-            FETCH cursorNum INTO rec;
-            EXIT WHEN NOT FOUND OR dupli = 1;
-            IF rec.idnumero = numdupli THEN
-                dupli = 1;
-            END IF;
-        END LOOP;
-        CLOSE cursorNum;
+		numdupli = uf || '9' || opera || random || num;
+		
+		-- Cursor para verificar se já existe o número na tabela
+		OPEN cursorNum NO SCROLL FOR SELECT idnumero FROM chip;
+		LOOP
+			FETCH cursorNum INTO rec;
+			EXIT WHEN NOT FOUND OR dupli = 1;
+			IF rec.idnumero = numdupli THEN
+				dupli = 1;
+			END IF;
+		END LOOP;
+		CLOSE cursorNum;
 END LOOP;
-
+-- Concatena as partes do numero
 numero = uf || '9' || opera || random || num;
 
 NEW.idnumero = numero;
@@ -58,7 +71,7 @@ RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
--- Cria a trigger
+-- Cria a trigger para inserir o idnumero na criação antes do insert no chip
 CREATE TRIGGER insereNum
 BEFORE INSERT  ON chip
 FOR EACH ROW
@@ -66,6 +79,9 @@ EXECUTE PROCEDURE insere_Num();
 
 -- Para observar o resultado use as linhas abaixo
 SELECT * FROM chip
-order by chip.idnumero
 INSERT INTO chip (idOperadora, idPlano, ativo, disponivel) VALUES ( 1, 1, 'N', 'S');
 INSERT INTO chip (idOperadora, idPlano, ativo, disponivel)  VALUES ( 2, 1, 'S', 'N');
+DELETE FROM CHIP WHERE IDPLANO = 1 AND ATIVO ='N'
+DELETE FROM CHIP WHERE IDPLANO = 1 AND IDOPERADORA = 2
+
+
