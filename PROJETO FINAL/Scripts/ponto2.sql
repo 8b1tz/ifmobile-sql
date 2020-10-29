104ac61417fe055502ee8dae72f6df27fb8ef2d5
Bielsaar <bielsaar@gmail.com>
2020-10-29 18:44:33 -0300

    Create ponto2.sql
    Incompleto

---
+++ PROJETO FINAL/Scripts/ponto2.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION verInativNum ()
+RETURNS TABLE (idnumeroF char(11), ativoF char(1), disponivelF char(1))
+AS $$
+DECLARE
+BEGIN
+-- aqui iremos retornar uma tabela que mostra chip que estão disponiveis para o uso e que não tem problemas técnicos;
+-- disponiveis -> disponivel ='S' e sem problemas -> ativo = 'S'
+RETURN QUERY SELECT idnumero, ativo,disponivel FROM chip WHERE ativo = 'S' AND disponivel ='S' limit 5;
+END; $$
+LANGUAGE 'plpgsql';
+
+drop function  verInativNum()
+
+SELECT * FROM verInativNum();
