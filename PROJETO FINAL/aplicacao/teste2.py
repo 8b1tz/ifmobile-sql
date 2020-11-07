2b326e3927d1d9b02cc1a9796eca8e53ec984d73
Bielsaar <bielsaar@gmail.com>
2020-11-06 23:38:24 -0300

    GUI in Tkinter  Experimental

---
+++ PROJETO FINAL/aplicacao/teste2.py
@@ -0,0 +1,182 @@
+try:
+    import tkinter as tk                # python 3
+    from tkinter import font as tkfont  # python 3
+except ImportError:
+    import Tkinter as tk     # python 2
+    import tkFont as tkfont  # python 2
+
+c = "Voltar ao Menu"
+class SampleApp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        for F in (StartPage, PageOne, PageTwo, PageThree, PageFour, PageFive, PageSix, PageSeven, PageEight, PageNine):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("StartPage")
+
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Escolha o que deseja.", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+
+        button1 = tk.Button(self, text="Geração de Numeros",
+                            command=lambda: controller.show_frame("PageOne"))
+        button2 = tk.Button(self, text="5 Numeros Aleatórios Disponiveis",
+                            command=lambda: controller.show_frame("PageTwo"))
+        button3 = tk.Button(self, text="Rotina de Fatura",
+                            command=lambda: controller.show_frame("PageThree"))
+        button4 = tk.Button(self, text="Testes Requisito 4",
+                            command=lambda: controller.show_frame("PageFour"))
+        button5 = tk.Button(self, text="Testes Requisito 5",
+                            command=lambda: controller.show_frame("PageFive"))
+        button6 = tk.Button(self, text="Rotina de Ligações",
+                            command=lambda: controller.show_frame("PageSix"))
+        button7 = tk.Button(self, text="V1 - Ranking",
+                            command=lambda: controller.show_frame("PageSeven"))
+        button8 = tk.Button(self, text="V2 - Faturamento",
+                            command=lambda: controller.show_frame("PageEight"))
+        button9 = tk.Button(self, text="V3 - Detalhes do Cliente",
+                            command=lambda: controller.show_frame("PageNine"))
+        button1.pack() 
+        button2.pack()
+        button3.pack()
+        button4.pack()
+        button5.pack()
+        button6.pack()
+        button7.pack()
+        button8.pack()
+        button9.pack()
+
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Geração de Numeros", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text=c,
+                        command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="5 Numeros Aleatórios Disponiveis", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text=c,
+                        command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+class PageThree(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Rotina de Fatura", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text=c,
+                        command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+class PageFour(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Testes Requisito 4", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text=c,
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+class PageFive(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Testes Requisito 5", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text=c,
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+class PageSix(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Rotina de Ligações", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text=c,
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+class PageSeven(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="V1 - Ranking", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text=c,
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+class PageEight(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="V2 - Faturamento", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text=c,
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+class PageNine(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="V3 - Detalhes do Cliente", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text=c,
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+if __name__ == "__main__":
+    app = SampleApp()
+    app.mainloop()
