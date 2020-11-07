73f4d228041a982c0c7d42daf06d4e0c698e37bd
Bielsaar <bielsaar@gmail.com>
2020-11-07 01:48:30 -0300

    Update teste2.py

--- PROJETO FINAL/aplicacao/teste2.py
+++ PROJETO FINAL/aplicacao/teste2.py
@@ -61,6 +61,67 @@ class List:
             print("taxa roaming: ",row[6])
             print("total: ",row[7])
             print("pago: ",row[8] )
+            print("===================" )
+
+    def negLigInat(self): 
+        cur = con.cursor()
+
+        cur.execute("select * from ligacao")
+        antiga_liga = cur.fetchall()
+        cur.execute("SELECT idNumero FROM chip where ativo = 'N' LIMIT 5;") 
+        result_inativos = cur.fetchall()
+        print("Números inativos: ")
+        for row3 in result_inativos:
+            print(row3)
+        cur.execute("SELECT idNumero FROM chip where ativo = 'S'LIMIT 5;") 
+        result_ativos = cur.fetchall()
+        print("Números ativos: ")
+        for row2 in result_ativos:
+            print(row2)
+        emissor = input("Insira o número que ligou: ")
+        receptor = input("Insira o receptor: ")
+        cur.execute("insert into ligacao (data, chip_emissor, ufOrigem, chip_receptor, ufDestino, duracao) values ('2001-07-27 14:11:00',%s, 'PB', %s, 'PB', '2:52:06');",(emissor, receptor))
+        con.commit()
+        cur.execute("select * from ligacao;")
+        novaliga = cur.fetchall()
+        lista_liga = [x for x in novaliga if x not in antiga_liga]
+        for row in lista_liga:
+            print("data: ", row[0])
+            print("emissor: ",row[1])
+            print("uf origem: ",row[2])
+            print("receptor:: ",row[3])
+            print("uf destino: ",row[4])
+            print("duracao: ",row[5])
+
+    def libChip(self): 
+        cur = con.cursor()
+        cur.execute ("SELECT idCliente from cliente where cancelado = 'N' LIMIT 5;")
+        result_naocanc1 = cur.fetchall()
+        print("Clientes não cancelados: ")
+        for row5 in result_naocanc1:
+            print(row5)
+        cliente1 = input("Qual cliente você deseja cancelar? ")
+        cur.execute("UPDATE cliente  SET cancelado = 'S' where idCliente = "+cliente1+";")
+        con.commit()
+
+    def negChCliIna(self):
+        cur = con.cursor()
+        cur.execute ("SELECT idCliente from cliente where cancelado = 'N' LIMIT 5;")
+        result_naocanc = cur.fetchall()
+        print("Clientes não cancelados: ")
+        for row4 in result_naocanc:
+            print(row4)
+        cur.execute ("SELECT idCliente from cliente where cancelado = 'S' LIMIT 5;")
+        result_canc = cur.fetchall()
+        print("Clientes Cancelados:")
+        for row5 in result_canc:
+            print(row5)
+        cliente = input("Digite o seu idCliente: ")
+        self.gera5NumDisp()
+        numero = input("Digite o seu número: ")
+        cur.execute("insert into cliente_chip (idNumero, idCliente) values ('"+numero+"', "+cliente+");")
+        con.commit()
+
 
     def povoaLig(self): 
         print('povoaLig')
@@ -86,7 +147,10 @@ class List:
          4 - View 1: Ranking planos
          5 - View 2: Faturamento por mes/ano
          6 - View 3: Detalhamento dos clientes
-         7 - Gera Fatura
+         7 - Negar Ligação de numeros Inativos/Disponiveis
+         8 - Liberar Chip de cliente cancelado
+         9 - Garante chip disponivel para cliente ativo
+         10 - Gera Fatura
         """)
 
 lis = List()
@@ -126,7 +190,17 @@ while (r != '0'):
 
     elif r == "6":
         lis.viewTres()
-    elif r == "7":
+        
+    elif r == '7':
+        lis.negLigInat()
+
+    elif r == "8":
+        lis.libChip()
+
+    elif r == "9":
+        lis.negChCliIna()
+
+    elif r == "10":
         print("Escreva em números, o mes: ")
         mes = int(input())
         print("Escreva em números, o ano: ")
