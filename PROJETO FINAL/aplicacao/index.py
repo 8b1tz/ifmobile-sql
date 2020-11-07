08441067d73ecb5dee67bcab668fed9e31601994
AnaJL <analinsdev@gmail.com>
2020-11-07 02:05:41 -0300

    melhorando ponto 6

--- PROJETO FINAL/aplicacao/index.py
+++ PROJETO FINAL/aplicacao/index.py
@@ -117,7 +117,16 @@ class List:
         for row5 in result_naocanc1:
             print(row5)
         cliente1 = input("Qual cliente você deseja cancelar? ")
-        cur.execute("UPDATE cliente  SET cancelado = 'S' where idCliente = "+cliente1+";")
+        cur.execute("SELECT chip.idnumero FROM cliente join cliente_chip on cliente_chip.idcliente = cliente.idcliente join chip on cliente_chip.idnumero = chip.idnumero WHERE cliente.idcliente = "+cliente1+";")
+        result_clch = cur.fetchall()
+        print("Números do cliente: ")
+        for row6 in result_clch:
+            print(row6)
+        resposta = print(input("Tem certeza que quer fazer isso? S/N: "))
+        if resposta == 'S' or 's':
+            cur.execute("UPDATE cliente  SET cancelado = 'S' where idCliente = "+cliente1+";")
+            print("Agora números estão disponiveis! ")
+
         con.commit()
 
     def negChCliIna(self):
@@ -179,10 +188,10 @@ while (r != '0'):
     elif r == "7": # QUASE OK
         lis.negLigInat()
 
-    elif r == "8":
-        lis.libChip()
+    elif r == "8": # QUASE OK
+        lis.libChip()  
 
-    elif r == "9":
+    elif r == "9": # QUASE OK
         lis.negChCliIna()
 
     elif r == "10": # QUASE OK
--- PROJETO FINAL/aplicacao/teste.py
+++ PROJETO FINAL/aplicacao/teste.py
@@ -8,11 +8,22 @@ con = psycopg2.connect(
     port = 5432
 )
 cur = con.cursor()
+
+
 cur.execute ("SELECT idCliente from cliente where cancelado = 'N' LIMIT 5;")
 result_naocanc1 = cur.fetchall()
 print("Clientes não cancelados: ")
 for row5 in result_naocanc1:
     print(row5)
 cliente1 = input("Qual cliente você deseja cancelar? ")
-cur.execute("UPDATE cliente  SET cancelado = 'S' where idCliente = "+cliente1+";")
+cur.execute("SELECT chip.idnumero FROM cliente join cliente_chip on cliente_chip.idcliente = cliente.idcliente join chip on cliente_chip.idnumero = chip.idnumero WHERE cliente.idcliente = "+cliente1+";")
+result_clch = cur.fetchall()
+print("Números do cliente: ")
+for row6 in result_clch:
+    print(row6)
+resposta = print(input("Tem certeza que quer fazer isso? S/N: "))
+if resposta == 'S' or 's':
+    cur.execute("UPDATE cliente  SET cancelado = 'S' where idCliente = "+cliente1+";")
+    print("Agora números estão disponiveis! ")
+
 con.commit()
