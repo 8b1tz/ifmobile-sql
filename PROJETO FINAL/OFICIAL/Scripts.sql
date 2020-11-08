1902b0815d2a2627fb5f1c5e16cf3e1157aafa98
Bielsaar <bielsaar@gmail.com>
2020-11-08 19:07:17 -0300

    Arquivo com todos os requisitos

--- PROJETO FINAL/OFICIAL/Scripts.sql
+++ PROJETO FINAL/OFICIAL/Scripts.sql
@@ -397,4 +397,44 @@ END; $$
 LANGUAGE 'plpgsql';
 
 
--- VIEW 1
+/*VISÃO 1*/
+CREATE VIEW rankPlan AS
+    (SELECT p.idplano,
+            p.descricao,
+            COUNT(c.*) AS quant, 
+            SUM(p.valor) AS total 
+    FROM plano p 
+    JOIN chip c 
+    ON p.idplano = c.idplano AND ativo = 'S'
+    GROUP BY p.idplano
+    ORDER BY quant DESC);
+
+
+
+/*VISÃO 2*/
+
+CREATE VIEW faturamento AS
+    (SELECT EXTRACT(YEAR FROM f.referencia) AS ano, 
+    EXTRACT(MONTH FROM f.referencia) AS mes,
+    COUNT(f.idnumero) AS numClientes, 
+    SUM(f.total) AS faturamento
+    FROM fatura f 
+    GROUP BY (ano, mes)
+    ORDER BY (faturamento) DESC );
+
+
+/*VISÃO 3*/
+CREATE VIEW fidelidade AS
+    (SELECT     cli.idcliente, 
+            cli.nome, 
+            ci.uf, 
+            clichi.idnumero, 
+            chip.idplano, 
+    extract('year' from age(CURRENT_DATE,cli.datacadastro)) || ' ano(s) ' || extract('month' from age(CURRENT_DATE,cli.datacadastro)) || ' mes(es) '  AS tempoFiel
+    FROM cliente cli 
+    JOIN cidade ci 
+    ON cli.idcidade = ci.idcidade 
+    JOIN cliente_chip clichi 
+    ON cli.idcliente = clichi.idcliente 
+    JOIN chip 
+    ON clichi.idnumero = chip.idnumero AND chip.ativo = 'S');
